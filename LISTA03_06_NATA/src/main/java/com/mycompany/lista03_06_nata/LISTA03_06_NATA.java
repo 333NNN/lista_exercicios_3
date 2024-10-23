b62efa13be0ee95f76d3070eb8578728fe8c4683
Natã Jordão <141784916+333NNN@users.noreply.github.com>
2024-10-23 20:03:36 -0300

    6. Média de Três Números

---
+++ LISTA03_06_NATA/src/main/java/com/mycompany/lista03_06_nata/LISTA03_06_NATA.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista03_06_nata;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ncunha
+ */
+public class LISTA03_06_NATA {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        
+        System.out.print("Digite a primeira nota: ");
+        float n1 = read.nextFloat();
+        System.out.print("Digite a segunda nota: ");
+        float n2 = read.nextFloat();
+        System.out.print("Digite a terceira nota: ");
+        float n3 = read.nextFloat();
+        float media = (n1 + n2 + n3)/3;
+        
+        System.out.println("Sua nota é: " + media);
+        if(media >= 6) {
+            System.out.println("Aluno aprovado.");
+        }
+        else {
+            System.out.println("Aluno reprovado.");
+        }
+        
+        
+    }
+}

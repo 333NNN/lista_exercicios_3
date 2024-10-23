9b63fb458e9200e1bfc90bcfdbf8485049ecbcdc
Natã Jordão <141784916+333NNN@users.noreply.github.com>
2024-10-23 20:46:54 -0300

    Add files via upload

---
+++ LISTA03_08_NATA/src/main/java/com/mycompany/lista03_08_nata/LISTA03_08_NATA.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.lista03_08_nata;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ncunha
+ */
+public class LISTA03_08_NATA {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.print("Digite a primeira nota: ");
+        float n1 = ler.nextFloat();
+        System.out.print("Digite a segunda nota: ");
+        float n2 = ler.nextFloat();
+        System.out.print("Digite a terceira nota: ");
+        float n3 = ler.nextFloat();
+        float media = (n1 + n2 + n3) / 3;
+
+        System.out.println("Sua nota é: " + media);
+        if (media >= 6) {
+            System.out.println("Aluno aprovado.");
+        } 
+        else {
+            System.out.println("Aluno reprovado.");
+        }
+    }
+}
